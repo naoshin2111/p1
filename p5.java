af2f515848ddd83f699fcabd2ddf06c3bc99b76b
naoshin2111 <naoshinhridi@gmail.com>
2023-10-19 16:58:33 +0600

    Push Commit to main

--- p5.java
+++ p5.java
@@ -1,2 +1,9 @@
 public class p5 {
+    public void print(){
+        System.out.println("Hello WOrld");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld");
+    }
 }
